8bc244bcd84a2b750b9ce3bd06dfc75d07a741a4
rnons <remotenonsense@gmail.com>
2013-07-23 23:34:00 +0800

    add main.hs

---
+++ main.hs
@@ -0,0 +1,53 @@
+import Data.Char (isDigit)
+import Options.Applicative
+
+import Radio
+import Radio.Douban
+import Radio.Jing
+
+data Options = Options
+    { optCommand    :: Command
+    } deriving (Eq, Show)
+
+data Command = DoubanRadio String
+             | JingRadio String
+    deriving (Eq, Show)
+
+
+optParser :: Parser Options
+optParser = Options 
+    <$> subparser ( command "douban"       (info doubanOptions
+                        (progDesc "Play douban.fm"))
+                 <> command "jing"         (info jingOptions
+                        (progDesc "Play jing.fm"))
+                  )
+
+main :: IO ()
+main = do
+    o <- execParser $ info (helper <*> optParser) 
+                           (fullDesc <> header "Lord: radio commander")
+    case optCommand o of
+        DoubanRadio param -> douban param
+        JingRadio param   -> jing param
+
+doubanOptions :: Parser Command
+doubanOptions = DoubanRadio <$> argument str ( metavar "[<channel_id> | <musician>]" )
+
+jingOptions :: Parser Command
+jingOptions = JingRadio <$> argument str ( metavar "<keywords>" )
+
+douban :: String -> IO ()
+douban p 
+    | isChId p = play (Cid $ read p) []
+    | otherwise = play (Musician p) []
+  where
+    isChId = and . fmap isDigit
+
+jing :: String -> IO ()
+jing p = do
+    print p
+    tok <- readToken p
+    case tok of
+        Just tok' -> play tok' []
+        _         -> print "Token is invalid or not exist"
+
