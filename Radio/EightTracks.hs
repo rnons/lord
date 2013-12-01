1bc1c5a7cffedf6f09e883d59595556f644ff788
rnons <remotenonsense@gmail.com>
2013-12-01 10:54:42 +0800

    [8tracks] `lord 8tracks listen` can take numerical mix_id or full_slug (web_path)

--- Radio/EightTracks.hs
+++ Radio/EightTracks.hs
@@ -16,8 +16,10 @@ import qualified Data.ByteString.Char8 as C
 import           Data.Maybe (fromJust)
 import           Data.Yaml hiding (decode)
 import           Data.CaseInsensitive (mk)
+import           Data.Char (isDigit)
 import           Data.Conduit (($$+-))
 import           Data.Conduit.Attoparsec (sinkParser)
+import qualified Data.List as L
 import           GHC.Generics (Generic)
 import           Network.HTTP.Types 
 import           Network.HTTP.Conduit
@@ -43,6 +45,8 @@ keyHdr = (mk "X-Api-Key", C.pack apiKey)
 
 type ETParam = Radio.Param EightTracks
 
+-- | Response from http://8tracks.com/sets/new.json
+-- `play_token` is the value of interest
 data PlaySession = PlaySession
     { play_token            :: String
     , status                :: String
@@ -50,6 +54,25 @@ data PlaySession = PlaySession
     , notices               :: Maybe String
     , api_version           :: Int
     } deriving (Show, Generic)
+instance FromJSON PlaySession
+
+-- | Response from /play.json /next.json
+-- `track` is the value of interest
+data PlayResponse = PlayResponse
+    { play_set               :: MixSet
+    , play_status            :: String
+    } deriving (Show, Generic)
+instance FromJSON PlayResponse where
+    parseJSON = genericParseJSON defaultOptions { fieldLabelModifier = drop 5 }
+
+data MixSet = MixSet
+    { at_beginning          :: Bool
+    , at_last_track         :: Bool
+    , at_end                :: Bool
+    , skip_allowed          :: Bool
+    , track                 :: EightTracks
+    } deriving (Show, Generic)
+instance FromJSON MixSet
 
 data EightTracks = EightTracks 
     { id                    :: Int
@@ -59,26 +82,17 @@ data EightTracks = EightTracks
     , release_name          :: String
     , url                   :: String
     } deriving (Show, Generic)
-
-data MixSet = MixSet
-    { at_beginning          :: Bool
-    , at_last_track         :: Bool
-    , at_end                :: Bool
-    , skip_allowed          :: Bool
-    , track                 :: EightTracks
-    } deriving (Show, Generic)
-
-data MixResponse = MixResponse
-    { mix_set               :: MixSet
-    , mix_status            :: String
-    } deriving (Show, Generic)
-
-instance FromJSON PlaySession
 instance FromJSON EightTracks
-instance FromJSON MixSet
 
-instance FromJSON MixResponse where
-    parseJSON = genericParseJSON defaultOptions { fieldLabelModifier = drop 4 }
+-- | Response from http://8tracks.com/mixes/14.json or
+-- http://8tracks.com/dp/electrominimalicious.json
+-- `info_mix` is the value of interest
+data MixInfo = MixInfo
+    { info_mix               :: Exp.Mix
+    , info_status            :: String
+    } deriving (Show, Generic)
+instance FromJSON MixInfo where
+    parseJSON = genericParseJSON defaultOptions { fieldLabelModifier = drop 5 }
 
 instance Radio.Radio EightTracks where
     data Param EightTracks = Token
@@ -90,7 +104,7 @@ instance Radio.Radio EightTracks where
 
     parsePlaylist val =
         case fromJSON val of
-            Success s -> [track $ mix_set s]
+            Success s -> [track $ play_set s]
             Error err -> error $ "Parse playlist failed: " ++ show err
 
     -- Request play.json will record current mix to listening history.
@@ -152,6 +166,7 @@ instance ToJSON ETParam
 
 instance NeedLogin EightTracks where
     createSession strMixId email pwd = do
+        mId <- getMixId strMixId
         initReq <- parseUrl rurl
         let req = initReq { method = "POST"
                           , queryString = renderSimpleQuery False query
@@ -166,13 +181,13 @@ instance NeedLogin EightTracks where
       where
         rurl = "http://8tracks.com/sessions.json"
         query = [ ("login", C.pack email), ("password", C.pack pwd) ]
-        mId = read strMixId :: Int
 
     data Config EightTracks = Config { eight :: ETParam } deriving Generic
 
     mkConfig = Config
 
-    readToken mid = do
+    readToken strMixId = do
+        mId <- getMixId strMixId
         home <- Radio.getLordDir
         let yml = home ++ "/lord.yml"
         exist <- doesFileExist yml
@@ -183,7 +198,7 @@ instance NeedLogin EightTracks where
                     Nothing -> error $ "Invalid YAML file: " ++ show conf
                     Just c -> 
                         case fromJSON c of
-                            Success tok -> return $ Just $ (eight tok) { mixId = read mid }
+                            Success tok -> return $ Just $ (eight tok) { mixId = mId }
                             Error err -> do
                                 print $ "Parse token failed: " ++ show err
                                 return Nothing
@@ -230,3 +245,30 @@ pprMixes mixes =
         putStrLn $ "    Tags: " ++ Exp.tag_list_cache m
         putStrLn ""
         )
+
+get :: FromJSON a => String -> IO a
+get rurl = do
+    initReq <- parseUrl rurl
+    let req = initReq { requestHeaders = [verHdr, keyHdr] }
+    val <- withManager $ \manager -> do
+        res <- http req manager
+        responseBody res $$+- sinkParser json
+
+    case fromJSON val of
+        Success v -> return v
+        Error err -> error err
+
+
+getMixId :: String -> IO Int
+getMixId m
+  | isNumerical m = return $ read m
+  | otherwise = do
+    let mixUrl = if domain `L.isPrefixOf` m
+                     then m
+                     else (domain ++)
+                          (if "/" `L.isPrefixOf` m then m else '/' : m)
+    mix <- get (mixUrl ++ ".json")
+    return $ Exp.id $ info_mix mix
+  where
+    isNumerical = and . fmap isDigit
+    domain = "http://8tracks.com"
